02b6834aa07b4c8ba422c8b3f271aa92d32bce4b
谢斌 <Traceless0929@outlook.com>
2018-03-01 13:47:13 +0800

    添加项目文件。

---
+++ Traceless.Api/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Traceless.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver
+                    = new Newtonsoft.Json.Serialization.DefaultContractResolver());//JSON首字母小写解决
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Traceless API"
+                });
+
+                //Determine base path for the application.  
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                //Set the comments path for the swagger json and ui.  
+                var xmlPath = Path.Combine(basePath, "Traceless.Api.xml");
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:50000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ApiName = "Traceless Api";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Traceless API V1");
+            });
+
+        }
+    }
+}
---
+++ Traceless.Auth/Config.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Traceless.Auth
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("TracelessApi", "Traceless API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    // secret for authentication
+                    //ClientSecrets =
+                    //{
+                    //    new Secret("secret".Sha256())
+                    //},
+
+                    // scopes that client has access to
+                    AllowedScopes = { "TracelessApi" }
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "admin",
+                    Password = "admin"
+                }
+            };
+        }
+
+    }
+}
